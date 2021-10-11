f54b324fc8642ff5e2590f4a8be3b2016d8de449
ShadowGMR <teachhaylim12@gmail.com>
2021-10-11 21:04:00 +0700

    Update index.dart

--- lib/screens/ShopDetail/index.dart
+++ lib/screens/ShopDetail/index.dart
@@ -13,78 +13,25 @@ class _ShopDetailState extends State<ShopDetail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          const SliverAppBar(
-            floating: false,
-            pinned: true,
-            snap: false,
-            backgroundColor: Colors.white,
-            foregroundColor: Colors.black,
-            title: Text("Shop Detail"),
-            expandedHeight: 200,
-            flexibleSpace: FlexibleSpaceBar(),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            margin: const EdgeInsets.all(8),
+            child: Column(
+              children: [
+                for (var i = 0; i < 16; i++)
+                  Container(
+                    margin: const EdgeInsets.only(top: 8, bottom: 8),
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.accents[i],
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+              ],
+            ),
           ),
-          SliverList(
-            delegate: SliverChildListDelegate([
-              // Container(
-              //   height: 200,
-              //   decoration: BoxDecoration(
-              //     color: Colors.white,
-              //   ),
-              // ),
-              for (var i = 1; i < 16; i++)
-                Container(
-                  color: Colors.accents[i],
-                  height: 100,
-                  width: double.infinity,
-                ),
-            ]),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class V1 extends StatelessWidget {
-  const V1({
-    Key? key,
-    required this.shops,
-  }) : super(key: key);
-
-  final List<String> shops;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        scrollDirection: Axis.vertical,
-        slivers: [
-          SliverList(
-            delegate: SliverChildListDelegate([
-              for (var item in shops)
-                Container(
-                  margin: const EdgeInsets.all(8),
-                  height: 80,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(6),
-                    boxShadow: [
-                      BoxShadow(
-                        offset: const Offset(0, 2),
-                        color: Colors.grey.shade200,
-                        blurRadius: 6,
-                      )
-                    ],
-                  ),
-                  child: Center(
-                    child: Text(item),
-                  ),
-                )
-            ]),
-          )
-        ],
+        ),
       ),
     );
   }
